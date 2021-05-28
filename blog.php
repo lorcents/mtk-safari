bfc4f009acbbe3829ec6f815012468c3a12da19b
lorcents <iannjoroge120@outlook.com>
2021-05-28 14:47:37 +0300

    blog

--- blog.php
+++ blog.php
@@ -57,7 +57,7 @@
     <div class="row justify-content-center">
       <!-- Column -->
       <div class="col-md-8 text-center">
-        <h3 class="my-3">Latest News and Blog</h3>
+        <h3 class="my-3">Latest News and Vlogs</h3>
         <h6 class="subtitle font-weight-normal">You can relay on our amazing features list and also our customer services will be great experience for you without doubt</h6>
       </div>
       <!-- Column -->
@@ -67,10 +67,10 @@
       <!-- Column -->
       <div class="col-lg-6">
         <div class="card border-0 mb-4">
-          <a href="#"><img class="card-img-top" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/blog/blog-home/img4.jpg" alt="wrappixel kit"></a>
-          <div class="date-pos text-center text-white p-3 bg-success-gradiant">JOHN DOE &nbsp; &nbsp; SEPT 15, 2017</div>
-          <h5 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">Techonologies started to change Drastically</a></h5>
-          <p class="m-t-20">You can relay on our amazing features list and also our customer services will be great experience. Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          <a href="#"><img class="card-img-top" src="assets/img/elephant.jpg" alt="wrappixel kit"></a>
+          <div class="date-pos text-center text-white p-3 bg-success-gradiant">The Big ive&nbsp; &nbsp; SEPT 15, 2020</div>
+          <h5 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">The Elephant</a></h5>
+          <p class="m-t-20">They are the world's largest terrestrial mammal. There are two recognized subspecies of this iconic animal: bush (or savanna) and fores </p>
         </div>
       </div>
       <!-- Column -->
@@ -79,33 +79,33 @@
           <!-- Column -->
           <div class="col-md-6">
             <div class="card border-0 mb-4">
-              <a href="#"><img class="card-img-top" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/blog/blog-home/img5.jpg" alt="wrappixel kit"></a>
-              <div class="date-pos text-center text-white p-3 bg-success-gradiant">JOHN DOE &nbsp; &nbsp; SEPT 15, 2017</div>
-              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">New Seminar on Newest Food Recipe</a></h6>
+            <iframe width="255" height="200" src="https://www.youtube.com/embed/3orZhdfp1Sg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <div class="date-pos text-center text-white p-3 bg-success-gradiant">Mtk Youtube&nbsp; &nbsp; OCT 30, 2020</div>
+              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">Ziplining In kenya</a></h6>
             </div>
           </div>
           <!-- Column -->
           <div class="col-md-6">
             <div class="card border-0 mb-4">
-              <a href="#"><img class="card-img-top" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/blog/blog-home/img6.jpg" alt="wrappixel kit"></a>
-              <div class="date-pos text-center text-white p-3 bg-success-gradiant">JOHN DOE &nbsp; &nbsp; SEPT 15, 2017</div>
-              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">New Seminar on Newest Food Recipe</a></h6>
+            <iframe width="255" height="200" src="https://www.youtube.com/embed/5vMDBB7wVzA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <div class="date-pos text-center text-white p-3 bg-success-gradiant">Mtk Youtube &nbsp; &nbsp; JUN 4, 2020</div>
+              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">Tips or safari</a></h6>
             </div>
           </div>
           <!-- Column -->
           <div class="col-md-6">
             <div class="card border-0 mb-4">
-              <a href="#"><img class="card-img-top" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/blog/blog-home/img7.jpg" alt="wrappixel kit"></a>
-              <div class="date-pos text-center text-white p-3 bg-success-gradiant">JOHN DOE &nbsp; &nbsp; SEPT 15, 2017</div>
-              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">New Seminar on Newest Food Recipe</a></h6>
+            <iframe width="255" height="200" src="https://www.youtube.com/embed/Ei-oFmNELuI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <div class="date-pos text-center text-white p-3 bg-success-gradiant">Mtk Youtube &nbsp; &nbsp; April 16, 2020</div>
+              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">Kakamega Forest</a></h6>
             </div>
           </div>
           <!-- Column -->
           <div class="col-md-6">
             <div class="card border-0 mb-4">
-              <a href="#"><img class="card-img-top" src="https://www.wrappixel.com/demos/ui-kit/wrapkit/assets/images/blog/blog-home/img8.jpg" alt="wrappixel kit"></a>
-              <div class="date-pos text-center text-white p-3 bg-success-gradiant">JOHN DOE &nbsp; &nbsp; SEPT 15, 2017</div>
-              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">New Seminar on Newest Food Recipe</a></h6>
+            <iframe width="255" height="200" src="https://www.youtube.com/embed/i8bTCPkpf_c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              <div class="date-pos text-center text-white p-3 bg-success-gradiant">Mtk Youtube &nbsp; &nbsp; July 16, 2017</div>
+              <h6 class="font-weight-medium mt-3"><a href="#" class="link text-decoration-none">Longest and ancient Bridge in the world</a></h6>
             </div>
           </div>
           <!-- Column -->
